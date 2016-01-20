288bef853580573f7e86eed1a7e7818196d449e5
ephys <ephyspotato@gmail.com>
2016-01-20 20:32:13 +0100

    range input style & firefox compat

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,21 @@
 'use strict';
 
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minify = require('gulp-cssnano');
 gulp.task('css', function() {
-	try {
-		gulp.src('./_assets/css/main.scss')
-			.pipe(sass())
-			.pipe(autoprefixer({
-				browsers: ['last 2 versions'],
-				cascade: false
-			}))
-			.pipe(minify())
-			.pipe(gulp.dest('./assets/css'));
-	} catch(e) {
-		console.log(e);
-	}
+	gulp.src('./_assets/css/main.scss')
+		.pipe(plumber())
+		.pipe(sass({errLogToConsole: true}))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(minify())
+		.pipe(gulp.dest('./assets/css'));
 });
 
 gulp.task('default', function() {
