5d629e6f59510f8fb2b6a9777100c91ad6bba03e
ephys <ephyspotato@gmail.com>
2016-01-21 13:15:13 +0100

    fix font size on desktop & added website screenshots

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+//	var footer = document.querySelector('body footer');
+//
+//	var shareButton = document.createElement('button');
+//	shareButton.textContent = 'Share';
+//
+//	var buttonWrapper = document.createElement('div');
+//	buttonWrapper.appendChild(shareButton);
+//	footer.appendChild(buttonWrapper);
+}, false);
